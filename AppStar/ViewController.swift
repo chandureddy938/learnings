f97a4531d8989c71341df48d4bcb81041b9a2334
Chandu Reddy😎 <chandureddy154@icloud.com>
2018-10-07 13:18:26 +0530

    Initial Commit

---
+++ AppStar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppStar
+//
+//  Created by Chandu Reddy on 07/10/18.
+//  Copyright © 2018 Chandu Reddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
